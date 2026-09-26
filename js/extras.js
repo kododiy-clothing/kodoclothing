e066032f4d990c27f3c94730214d60344d259031
sumitkhatod1990 <168472799+sumitkhatod1990@users.noreply.github.com>
2026-09-26 17:52:45 +0530

    Disable product photo shuffle

--- js/extras.js
+++ js/extras.js
@@ -12,7 +12,6 @@
     initVisualSizeChartModal();
     initKodoStylistBot();
     initMobileBottomDock();
-    initProductPhotoRotator();
   });
 
   /* -------------------------------------------------------------
