6d75b109226a94f28f2a7322d66bc4aa0446efb7
sumitkhatod1990 <168472799+sumitkhatod1990@users.noreply.github.com>
2026-09-26 10:49:22 +0530

    Configure Google OAuth client for admin

--- js/backend-config.js
+++ js/backend-config.js
@@ -4,4 +4,4 @@ window.KODO_API_BASE_URL = window.KODO_API_BASE_URL || "";
 
 // Add the Google OAuth Web Client ID here after creating it in Google Cloud.
 // Admin will stay locked until this is configured, avoiding fake demo sign-in.
-window.KODO_GOOGLE_CLIENT_ID = window.KODO_GOOGLE_CLIENT_ID || "";
+window.KODO_GOOGLE_CLIENT_ID = window.KODO_GOOGLE_CLIENT_ID || "315340311965-akvp9jp8ug8g24h0236mb68k2iot9frp.apps.googleusercontent.com";
